8fb0d62220553a73b7636111158cea5b031e019a
VanTay2k1 <nguyenvantay0601@gmail.com>
2022-04-05 09:16:46 +0700

    first 1

--- Foreach,Enum,Truct/Foreach,Enum,Truct/Program.cs
+++ Foreach,Enum,Truct/Foreach,Enum,Truct/Program.cs
@@ -8,62 +8,102 @@ namespace Foreach_Enum_Truct
 {
     class Program
     {
+        enum dienSinhVien
+        {
+            hoNgheo,
+            hoCanNgheo,
+            binhThuong,
+        }
         struct SinhVien
         {
-            public int msv;
-            public string ten;
-            public double DiemToan;
-            public double DiemLy;
-            public double DiemHoa;
-            public double DiemTB;
-
-        }
-        public void NhapSinhVien()
-        {
-            
-            SinhVien sv = new SinhVien();
-            Console.Write("Nhap ma sinh vien: ");
-            sv.msv = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Nhap ten sinh vien: ");
-            sv.ten = Convert.ToString(Console.ReadLine());
-
-            Console.Write("Nhap diem toan: ");
-            sv.DiemToan = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Nhap diem ly: ");
-            sv.DiemLy = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Nhap diem hoa: ");
-            sv.DiemHoa = Convert.ToDouble(Console.ReadLine());
-
-            TinhDTB(sv);
-        }
-        private void TinhDTB(SinhVien sv)
-        {
-            double DiemTB = (sv.DiemToan + sv.DiemLy + sv.DiemHoa) / 3;
-            sv.DiemTB = Math.Round(DiemTB, 2, MidpointRounding.AwayFromZero);
+            public int MaSV_150;
+            public string HoTen_150;
+            public double DiemToan_150;
+            public double DiemLy_150;
+            public double DiemVan_150;
+            public int dienSinhVien_150;
         }
 
-        public void ShowSinhVien(List<SinhVien> listSV)
+        static void NhapThongTinSinhVien(out SinhVien SV)
         {
-            
-            Console.WriteLine("{0, -5} {1, -20} {4, 5} {5, 5} {6, 5} {7, 10}",
-                  "MSV", "Tên", "Toan", "Ly", "Hoa", "Diem TB");
+            Console.Write(" Ma SV: ");
+            SV.MaSV_150 = int.Parse(Console.ReadLine());
+            Console.Write(" Ho ten: ");
+            SV.HoTen_150 = Console.ReadLine();
+            Console.Write(" Diem toan: ");
+            SV.DiemToan_150 = Double.Parse(Console.ReadLine());
+            Console.Write(" Diem ly: ");
+            SV.DiemLy_150 = Double.Parse(Console.ReadLine());
+            Console.Write(" Diem van: ");
+            SV.DiemVan_150 = Double.Parse(Console.ReadLine());
+            Console.WriteLine(" Dien Sinh Vien: 0:hoCanNgheo, 1:hoNgheo, 2:binhThuong ");
+            SV.dienSinhVien_150 = int.Parse(Console.ReadLine());
+        }
 
-            if (listSV != null && listSV.Count > 0)
+        static void XuatThongTinSinhVien(SinhVien SV)
+        {
+            Console.WriteLine(" Ma so: " + SV.MaSV_150);
+            Console.WriteLine(" Ho ten: " + SV.HoTen_150);
+            Console.WriteLine(" Diem toan: " + SV.DiemToan_150);
+            Console.WriteLine(" Diem ly: " + SV.DiemLy_150);
+            Console.WriteLine(" Diem van: " + SV.DiemVan_150);
+            if (SV.dienSinhVien_150 == (int)dienSinhVien.hoNgheo)
             {
-                foreach (SinhVien sv in listSV)
-                {
-                    Console.WriteLine("{0, -5} {1, -20} {4, 5} {5, 5} {6, 5} {7, 10}",
-                                      sv.msv, sv.ten, sv.DiemToan, sv.DiemLy, sv.DiemHoa,
-                                      sv.DiemTB);
-                }
+                Console.WriteLine(" Thuoc dien: Ho Ngheo");
             }
-            Console.WriteLine();
+            else if (SV.dienSinhVien_150 == (int)dienSinhVien.hoCanNgheo)
+            {
+                Console.WriteLine(" Thuoc dien: Ho Can Ngheo");
+            }
+            else
+            {
+                Console.WriteLine(" Thuoc dien: Binh Thuong");
+            }
         }
+
+        static double DiemTBSinhVien(SinhVien SV)
+        {
+            return (SV.DiemToan_150 + SV.DiemLy_150 + SV.DiemVan_150) / 3;
+        }
+
         static void Main(string[] args)
         {
+
+
+            int n_150;
+            do
+            {
+                Console.Write("Nhap SL sinh vien: ");
+                n_150 = int.Parse(Console.ReadLine());
+            } while (n_150 <= 0);
+            SinhVien[] sv150 = new SinhVien[n_150];
+            for (int i = 0; i < n_150; i++)
+            {
+                Console.Write("Nhap vao SV[{0}]: \n", i + 1);
+                NhapThongTinSinhVien(out sv150[i]);
+            }
+
+            Console.WriteLine("DANH SACH DUYEN SINH VIEN: ");
+            int d = 0;
+            foreach (SinhVien sv in sv150)
+            {
+                Console.WriteLine("\nSinh vien thu {0}: \n", d + 1);
+                XuatThongTinSinhVien(sv);
+                d++;
+            }
+
+
+            string searchSV_150;
+            Console.Write("\nNhap vao ten sv can tim kiem: ");
+            searchSV_150 = Console.ReadLine();
+            foreach (SinhVien sv in sv150)
+            {
+                if (sv.HoTen_150 == searchSV_150)
+                {
+                    Console.WriteLine("Diem TB cua sinh vien {0} la: {1} ", searchSV_150, DiemTBSinhVien(sv));
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
